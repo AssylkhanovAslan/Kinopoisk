478bf4bedcc57fe17c3d09cefa3fdad2d1aa3511
btokmyrza <btokmyrza@gmail.com>
2022-06-28 15:02:44 +0600

    first commit

--- app/src/main/java/com/example/kinoposik/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/kinoposik/presentation/main/MainViewModel.kt
@@ -1,31 +1,41 @@
 package com.example.kinoposik.presentation.main
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.kinoposik.data.MovieRepository
-import com.example.kinoposik.domain.models.MovieCategory
-import com.example.kinoposik.domain.models.tmdb.TmdbMovie
+import com.example.kinoposik.common.Constants.API_KEY
+import com.example.kinoposik.data.remote.dto.movie_details.MovieDetails
+import com.example.kinoposik.data.repository.MovieRepositoryImpl
+import com.example.kinoposik.data.remote.dto.popular_movies.toMovie
+import com.example.kinoposik.domain.entities.Movie
 import kotlinx.coroutines.launch
 
 class MainViewModel(
-    private val repository: MovieRepository
+    private val repository: MovieRepositoryImpl
 ) : ViewModel() {
 
-    val TAG = this::class.java.simpleName
+    private val _popularMovies = MutableLiveData<List<Movie>>()
+    val popularMovies: LiveData<List<Movie>> = _popularMovies
 
-    val movieCategories = MutableLiveData<List<TmdbMovie>>()
-    val userName = MutableLiveData<String>()
+    private val _movieDetails = MutableLiveData<MovieDetails>()
+    val movieDetails: LiveData<MovieDetails> = _movieDetails
 
     init {
         loadPopularMovies()
     }
-    fun loadPopularMovies() {
+
+    private fun loadPopularMovies() {
         viewModelScope.launch {
-            val results = repository.getPopularMovies()
-            movieCategories.value = results.tmdbMovies
-            Log.d(TAG, "Total pages = ${results.total_pages}, Total Results = ${results.total_results}")
+            _popularMovies.postValue(repository.getPopularMovies(API_KEY).results.map { it.toMovie() })
+        }
+    }
+
+    fun loadMovieDetails(id: Int) {
+        viewModelScope.launch {
+            val movieDetails = repository.getMovieDetails(id, API_KEY)
+            _movieDetails.postValue(movieDetails)
         }
     }
 
